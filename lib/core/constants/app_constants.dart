a577d3eea02a8da0057dab4632bdae4206e687b5
Mohammed Yazji <mw.alyazji@icloud.com>
2026-05-27 21:53:01 +0300

    feat: update base URL to use API v1

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   //private we can't make instance from this class just access static methods and proprieties
   AppConstants._();
 
-  static const String baseUrl = 'http://127.0.0.1:8000/api';
+  static const String baseUrl = 'http://127.0.0.1:8000/api/v1';
 
   // === Storage Keys ===
   static const String tokenKey = 'auth_token';
